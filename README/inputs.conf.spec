f905c54d270401b3300c56ce02a6a9f783c9c913
Zack Shainsky <zshainsky@splunk.com>
2015-07-28 22:44:33 -0500

    Phil: Please pull down the changes and note what I have done. You should see the input in the Settings > Data Inputs > Chatchpoint Modular Input in the Splunk UI.
    Committing modular input that I created. I have made some edits to the structure. With the consumer_key and consumer_secret stored in the setup page then there is only a need for the test_id to be in the modular input. I have made the changes accordingly.
    
    This commit is a commit only for the purpose of the adjustment in the structure of the modular input input fields.
    
    Issues: having issues indexing data into splunk. This will take some debugging but shouldn't be too difficult to work out this bug.

--- README/inputs.conf.spec
+++ README/inputs.conf.spec
@@ -1,4 +1,2 @@
 [app_catchpoint://<name>]
-consumer_key = <value>
-consumer_secret = <value>
 test_id = <value>
--- bin/app_catchpoint.py
+++ bin/app_catchpoint.py
@@ -20,25 +20,6 @@ class MIClass(Script):
 		scheme.use_external_validation = True
 		scheme.use_single_instance = True
 
-		# overview: defining 'arguments' below -- text fields which drive functionality
-		consumer_key_arg = Argument(
-			name="consumer_key",
-			description="Receives client ID here.",
-			# validation = "",
-			data_type=Argument.data_type_string,
-			required_on_edit=True,
-			required_on_create=True
-		)
-
-		consumer_secret_arg = Argument(
-			name="consumer_secret",
-			description="Receives client secret here.",
-			# validation = "",
-			data_type=Argument.data_type_string,
-			required_on_edit=True,
-			required_on_create=True
-		)
-
 		test_arg = Argument(
 			name="test_id",
 			description="The test ID(s)",
@@ -48,18 +29,14 @@ class MIClass(Script):
 			required_on_create=True
 		)
 
-		scheme.add_argument(consumer_key_arg)
-		scheme.add_argument(consumer_secret_arg)
 		scheme.add_argument(test_arg)
 		return scheme
 
 	# overview: check against lamba -- the empty string, review more eloquent solutions later
 	def validate_input(self, validation_definition):
-		consumer_key = validation_definition.parameters["consumer_key"]
-		consumer_secret = validation_definition.parameters["consumer_secret"]
 		test_id = validation_definition.parameters["test_id"]
 
-		if not consumer_key or not consumer_secret or not test_id:
+		if not test_id:
 			raise ValueError("All fields must have a value")
 
 	def stream_events(self, inputs, ew):
@@ -72,7 +49,8 @@ class MIClass(Script):
 
 		event_data = Event(
 			index='catchpoint',  # assuming container element which holds all events.
-			sourcetype='json'
+			sourcetype='json',
+			source='catchpoint_input'
 		)
 
 
@@ -84,16 +62,18 @@ class MIClass(Script):
 		consumer_key = setup_input['client_key']
 		consumer_secret = setup_input['client_secret']
 
-		# Keep for testing:
+		# Testing:
 		# event_data.data = cp_object.retrieve_rd_wrapper('RY-Rc-jSl18UYU23', '59d65360-9248-410e-a697-28e62b70054e', 81093)
 		# consider writing driver retrieve interface to accept variant key / secret / tests. -- update: done.
 
 		for input_name, input_item in inputs.inputs.iteritems():
 			test_id = input_item['test_id']
 
-			event_data.stanza = input_name
+			event_data.stanza = "stanza.stanza"
 			event_data.data = cp_object.retrieve_rd_wrapper(consumer_key, consumer_secret, test_id)
-			print event_data.data
+
+			# Testing:
+			# print event_data.data
 			# ew.write_event(event_data)
 
 		# consider writing driver retrieve interface to accept variant key / secret / tests. -- update: done.
